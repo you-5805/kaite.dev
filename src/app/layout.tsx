12ff5a2939167be4dd054694f7501d933a0c0153
Yo Iwamoto <you.iwamoto918@gmail.com>
2023-05-06 18:26:15 +0900

    fix: meta service description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,15 +5,15 @@ import type { PropsWithChildren } from 'react';
 import '@/styles/global.css';
 
 export const metadata = {
-  title: 'kaite.dev - 書いてほしい技術記事をリクエスト',
+  title: 'kaite.dev - 「書いて」を伝える',
   description:
-    'kaite.dev は、誰かに書いてほしい技術記事をリクエストするための Web サイトです。',
+    'kaite.dev は、ユーザーの「書きたい」記事のアイデアに、「書いて」を伝えられる Web サービスです。',
 } satisfies Metadata;
 
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang='ja'>
-      <body suppressHydrationWarning={true}>
+      <body suppressHydrationWarning>
         <Header />
 
         <div className='min-h-screen'>{children}</div>
